023bb0c87d8d4a90e9aa7f1630836a6a2689b6b0
angelinaheusler <aheusler@student.ethz.ch>
2018-11-29 20:18:22 +0100

    factorial

--- factorial.cpp
+++ factorial.cpp
@@ -1,1 +1,35 @@
 #include <iostream>
+
+typedef unsigned int uint_type;
+
+template <uint_type n>
+class factorial {
+public:
+	enum { value = n * factorial<n - 1>::value };
+};
+
+template<> class factorial<1> {
+public:
+	enum { value = 1 };
+};
+
+template <uint_type n, uint_type k>
+class bin {
+public:
+	enum { value = bin<n-1, k-1>::value + bin<n-1, k>::value };
+};
+
+template<uint_type n>
+struct bin<n, 0> {
+	enum { value = 1 };
+};
+
+template<uint_type n>
+struct bin <n, n> {
+	enum { value = 1 };
+};
+
+int main() {
+	std::cout << factorial<4>::value << std::endl << bin<4, 3>::value <<std::endl;
+	return 0;
+}
